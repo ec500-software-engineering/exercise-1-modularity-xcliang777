19f7228a76df22027e55912c2bdbb3a1d6e3e8ba
WenjieLuo2333 <43015006+WenjieLuo2333@users.noreply.github.com>
2019-02-05 23:12:43 -0500

    Update Alert_module.py

--- Alert_module.py
+++ Alert_module.py
@@ -1,63 +1,27 @@
-#It's the I/O documentation for the Health_Monitor_system
-#Especially for Alert System
-
-#Wire Definition
-
-#Input Analyzer
-# Input -- Data from sensors.
-
-def Input_Analyzer_get_data():
-	"""
-	receive data from monitor devices
-	Format: ((tuple),int type_of_data)
-	integer type_of_data represent the source of the data.(blood oxygen or pressure or else)
-	(tuple) defines the data we get from sensors
-	"""
-
-#Storage System
-def Storage_sys_get_data((data_pakage),type_of_data):
-	"""
-	Save the data_pakage into three different table according to type_of_data
-	Format: 
-	type_of_data:
-		blood oxygen: (double percent,time time)
-		blood presure: (double pressure,time time)
-		pulse: (double frequency, time time)
-	"""
-
-#AI Module
-def AI_module_input(bo,bp,pul)：
-	"""
-	bo,bp,pul are three lists represent the value of blood oxygen,blood pressure and pulse
-	bo,bp,pul: list [double value]
-	"""
-
-def AI_module_output():
-	"""
-	output format:
-		a double value for estimated health score
-	"""
-
-#User Interface
-def get_data():
-	"""
-	Get_data_from alert sys
-		format: three flags to trigger alert display
-
-	Get_data_from_data_base:
-		format:double values to display on screen
-	"""
+#It's the I/O documentation for Alert System
 
 #Alert Sys
-def Alert_input():
+def Alert_for_three_categories_input(data_in):
 	"""
 	get data for each type from database.
 	format:
 		(double value,int type)
 	"""
+	average_list = [[] for i in range(3)]
+	if len(average_list[data_in[1]]) < 20:
+		average_list[data_in[1]].append(data_in[0])
+	else:
+		del(average_list[data_in[1]][0])
+		average_list[data_in[1]].append(data_in[0])
+	if len(average_list[data_in[1]]) > 20 and exceed_threshold(mean(average_list[data_in[1]])):
+		alert_flag[data_in[1]] = 1
 
 def Alert_Output():
 	"""
 	Compare data with certain threthold
 	send flags to user interface module.
 	"""
+	listen_on(alert_flag);
+	if find_alert(alert_flag) != -1:
+		alert_type = alert_flag;
+		send_alert_message(alert_type) #send message to display devices and alarm alert
