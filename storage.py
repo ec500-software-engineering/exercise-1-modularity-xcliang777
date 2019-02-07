943cf191a2a6fa456ae16dfbf3bd4081f61e72d6
Gang Wei <43114190+WayGang@users.noreply.github.com>
2019-02-06 20:42:56 -0500

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,32 +1,22 @@
 class storage():
-    while 1:
-        def filter(self):
-            #for useful data
-        def tuple(self,bm,bo,p):
-            try:
-            #check if it is correct
-                tup = ('bloodmeasure','bloodoxygen','pulse',bm,bo,p)
-                return tup
-            except:
-                return 1
+    def __init__(self,bo,bp,pul):
+        self.bo = bo
+        self.bp = bp
+        self.pul = pul
+    def filter(self):
+        return 0
+        #for useful data
+    Input = input()
+    # connection to the database
+    # storage the data into the database
+    # extract the data out of the database of the format
+    def read(self):
+        if self.Input == "bo":
+            return self.bo
+        elif self.Input == "bp":
+            return self.bp
+        elif self.Input == "pul":
+            return self.pul
 
-        def input(bo,bp,pul):
-            #connection to the database
-            '''
-            format:
-            bo,bp,pul are three lists represent the value of blood oxygen,blood pressure and pulse
-            :param bp:
-            :param pul:
-            :return:
-            '''
-            #storage the data into the database
-        def output(bo,bp,pul):
-            #connection to the database
-            '''
-            format:
-            bo,bp,pul are three lists represent the value of blood oxygen,blood pressure and pulse
-            :param bp:
-            :param pul:
-            :return:
-            '''
-            #extract the data out of the database of the format
+    # for example: print(storage(3,4,5).read())
+    # which is bo = 3,bp = 4,and pul = 5
