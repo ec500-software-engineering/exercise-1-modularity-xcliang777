232dc9db55e174eeb940d2cc78f228a8bf244fdb
xcliang777 <43055334+xcliang777@users.noreply.github.com>
2019-02-10 18:14:23 -0500

    Add files via upload

---
+++ InputModule_lxc.py
@@ -0,0 +1,39 @@
+#Copyright Xiaocheng Liang xcliang@bu.edu
+
+'''
+Expecting receive data from monitor devices(blood oxygen, blood pressure, pulse)
+Now receive data from other source file(txt file)
+Format: float
+float type_of_data represent the source of the data.(blood oxygen or pressure or else)
+'''
+
+# class InputModule():
+# 	def __init__(self, bo, bp, pul):
+# 		self.bo = bo
+# 		self.bp = bp
+# 		self. pul = pul
+
+def input(path):
+	#Since we do not have data from the sensor now, we assuse that data is already in txt file
+	try:
+		with open(path, 'r') as f:
+			read = f.read().split()
+
+	except:
+		print("Reading data error")
+
+	if len(read)==0:
+		print("empty data")
+	res = [float(i) for i in read]
+
+	return res
+
+'''
+pathbo="./examplebo.txt"
+pathbp="./examplebp.txt"
+pathpul="./examplepul.txt"
+bo = input(pathbo)
+bp = input(pathbp)
+pul = input(pathpul)
+'''
+#print(bo)
