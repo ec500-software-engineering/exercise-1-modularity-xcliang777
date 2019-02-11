40c6c63be36f5e32c6460de2c8151770588cde6c
Qinmei Du <duqinmei@bu.edu>
2019-02-10 23:08:47 -0500

    user_interface update

--- UserInterface_module.py
+++ UserInterface_module.py
@@ -12,19 +12,31 @@ class userInterface():
         self.operation = []
 
     def getFromAlert(self, boalert, bpalert, pulalert):
+        """
+        Get_data_from_alert_sys:format: three flags from alert sys output to trigger alert display
+        """
         self.boalert = boalert
         self.bpalert = bpalert
         self.pulalert = pulalert
 
     def getFromData(self, bo, bp, pul):
+        """
+        Get_data_from_data_base: format:(double value, int type)
+        """
         self.bo = bo
         self.bp = bp
         self.pul = pul
 
     def getFromUser(self, operation):
+        """
+        Get_data_from_user: format: boolean control from user. Such as turn on, turn off user log in information
+        """
         self.operation = operation
 
     def sendToShow(self):
+        """
+        send input to electron device to show on the screen
+        """
         send_data = {
             self
         }
