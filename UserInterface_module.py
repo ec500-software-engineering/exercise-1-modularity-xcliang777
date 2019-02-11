ad88db2a7af269488eb5d711119e9951f1fc2837
Qinmei Du <duqinmei@bu.edu>
2019-02-10 22:52:15 -0500

    Update UserInterface_module.py

--- UserInterface_module.py
+++ UserInterface_module.py
@@ -1,18 +1,33 @@
 #It's the I/O documentation for the User_Interface_system
 #Qinmei Du
 #User Interface
-def userinterface_input():
-	"""
-	Get_data_from_data_base:
-		format:(double value, int type)
-	Get_data_from_alert_sys:
-		format: three flags from alert sys output to trigger alert display
-	Get_data_from_user:
-	    format: boolean control from user. Such as turn on, turn off
-	            user log in information
-	"""
+class userInterface():
+    def __init__(self):
+        self.bo = []
+        self.bp = []
+        self.pul = []
+        self.boalert = []
+        self.bpalert = []
+        self.pulalert = []
+        self.operation = []
 
-def userinterface_output():
-    """
-    send input to electron device to show on the screen
-    """
+    def getFromAlert(self, boalert, bpalert, pulalert):
+        self.boalert = boalert
+        self.bpalert = bpalert
+        self.pulalert = pulalert
+
+    def getFromData(self, bo, bp, pul):
+        self.bo = bo
+        self.bp = bp
+        self.pul = pul
+
+    def getFromUser(self, operation):
+        self.operation = operation
+
+    def sendToShow(self):
+        send_data = {
+            self
+        }
+        print(send_data)
+        return send_data
+
