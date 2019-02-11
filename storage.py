669524f99e4fb39a34d0b2d95a93abc2294857e5
Gang Wei <43114190+WayGang@users.noreply.github.com>
2019-02-10 22:10:53 -0500

    Update storage.py

--- storage.py
+++ storage.py
@@ -20,3 +20,27 @@ class storage():
 
     # for example: print(storage(3,4,5).read())
     # which is bo = 3,bp = 4,and pul = 5
+#another version:
+'''
+class storage():
+    def __init__(self,bo,bp,pul):
+        self.bo = bo
+        self.bp = bp
+        self.pul = pul
+    def filter(self):
+        return 0
+    #for useful data
+    # connection to the database
+    # storage the data into the database
+    # extract the data out of the database of the format
+    def read(self,Input):
+        if Input == "bo":
+            return self.bo
+        elif Input == "bp":
+            return self.bp
+        elif Input == "pul":
+            return self.pul
+
+    # for example: print(storage(3,4,5).read())
+    # which is bo = 3,bp = 4,and pul = 5
+'''
