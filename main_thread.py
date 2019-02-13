10a4d403bfb072f91279b4350df1a28a897954f3
xcliang <xcliang@bu.edu>
2019-02-13 12:52:58 -0500

    update thread

---
+++ main_thread.py
@@ -0,0 +1,46 @@
+#Copyright Xiaocheng Liang  xcliang@bu.edu
+
+import InputModule_lxc
+import Alert_module
+import AiModule
+import UserInterface_module
+import time
+import random
+import threading
+
+
+def run(bo,bp, pul):
+	'''''
+	AI Module
+	'''
+	robot = AiModule.AiModule()
+	robot.input_check(bo, bp, pul)
+	predBloodOxygen, predBloodPressure, prePulse = robot.predict()
+
+	'''
+	Alert Module
+	'''
+	Alt = Alert_module.Alert()
+	for k in range(len(bo)):
+	    boi = bo[k],0
+	    bpi = bp[k],1
+	    puli = pul[k],2
+	    boa = Alt.Alert_for_three_categories_input(boi)
+	    bpa = Alt.Alert_for_three_categories_input(bpi)
+	    pula = Alt.Alert_for_three_categories_input(puli)
+
+	'''''
+	UserInterface Module
+	'''''
+	User = UserInterface_module.userInterface()
+	User.getFromData(bo, bp, pul)
+
+while 1:
+	bo = InputModule_lxc.readdata("./examplebo.txt")
+	bp = InputModule_lxc.readdata("./examplebp.txt")
+	pul = InputModule_lxc.readdata("./examplepul.txt")
+	t1 = threading.Thread(target = run, args=(bo, bp, pul,))
+	t2 = threading.Thread(target = run, args=(bo, bp, pul,))
+	t1.start()
+	t2.start()
+	time.sleep(random.randint(2,4))
