1440296a4ec88135798824130a91e80ee7de12b2
xcliang777 <43055334+xcliang777@users.noreply.github.com>
2019-02-14 18:43:34 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='modularity',
+    version='0.0.1',
+    packages=['numpy'],
+    url='',
+    license='',
+    author='Xiaocheng Liang',
+    author_email='xcliang@bu.edu',
+    description=''
+)
